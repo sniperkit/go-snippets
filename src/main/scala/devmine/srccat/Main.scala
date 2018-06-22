1aadb1936830ef785a7859e3ef5b2be5c02d12a1
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-22 14:03:14 +0200

    Squashed '.snippets/github.com/DevMine/srccat/' content from commit 7a1bc6d3b2
    git-subtree-dir: .snippets/github.com/DevMine/srccat
    git-subtree-split: 7a1bc6d3b200b8010780ed861be5cab4fe868308

---
+++ src/main/scala/devmine/srccat/Main.scala
@@ -0,0 +1,76 @@
+package devmine.srccat
+
+import java.nio.file.{ Path, Files, Paths }
+import java.util.concurrent.Executors
+
+import akka.actor.ActorSystem
+import devmine.srccat.actors.Worker.Begin
+import devmine.srccat.actors.{ Coordinator, Worker }
+import scopt.OptionParser
+
+import scala.concurrent.ExecutionContext
+
+/**
+ * Main object of concat project
+ */
+object Main {
+
+  @SuppressWarnings(Array("org.brianmckenna.wartremover.warts.DefaultArguments"))
+  case class Config(repoRoot: String = "", outputFolder: String = "", parallelism: Int = Utils.DEFAULT_PARALLELISM)
+
+  def concat(repoRoot: Path, outputFolder: Path, parallelism: Int): Unit = {
+    val numWorkers = parallelism
+    val threadPool = Executors.newFixedThreadPool(numWorkers)
+    val executionContext = ExecutionContext.fromExecutor(threadPool)
+
+    /* Create new actor system */
+    val system = ActorSystem("srccat", defaultExecutionContext = Some(executionContext))
+
+    /* Initiate devmine.srccat.actors */
+    val master = system.actorOf(Coordinator.props(repoRoot, outputFolder, numWorkers))
+    val workers = Vector.fill(numWorkers)(system.actorOf(Worker.props(master)))
+
+    /* Start working */
+    workers.foreach {
+      _ ! Begin
+    }
+
+    /* Wait for termination */
+    system.awaitTermination()
+
+    threadPool.shutdown()
+  }
+
+  def main(args: Array[String]) {
+    import Files._
+
+    @SuppressWarnings(Array("org.brianmckenna.wartremover.warts.NonUnitStatements"))
+    val parser: OptionParser[Config] = new OptionParser[Config]("srccat") {
+      opt[Int]('j', "jobs").text("Maximum number of jobs to run").action((j, c) => c.copy(parallelism = j))
+      arg[String]("<REPO_ROOT>").text("Repository root").action((repo, c) => c.copy(repoRoot = repo))
+      arg[String]("<OUTPUT_FOLDER>").text("Output folder for big files").action((out, c) => c.copy(outputFolder = out))
+    }
+
+    def fail(msg: String): Nothing = {
+      Console.err.println(s"[srccat] ERROR : $msg")
+      sys.exit(1)
+    }
+
+    val conf = parser.parse(args, Config()) getOrElse sys.exit(1)
+
+    val repoRoot = Paths.get(conf.repoRoot)
+    val outputFolder = Paths.get(conf.outputFolder)
+
+    if (!isDirectory(repoRoot)) fail("Repository root is not a directory")
+    if (!isDirectory(outputFolder)) fail("Output folder is not a directory")
+
+    if (!outputFolder.toFile.list.isEmpty) fail("Output folder is not empty")
+
+    repoRoot.toFile.listFiles.filterNot(_.isDirectory).foreach[Unit] { file =>
+      fail(s"Found $file in the repository root which is not a directory!")
+    }
+
+    concat(repoRoot, outputFolder, conf.parallelism)
+  }
+}
+
---
+++ src/test/scala/SystemSpec.scala
@@ -0,0 +1,86 @@
+import java.nio.file.Files
+
+import de.svenjacobs.loremipsum.LoremIpsum
+import devmine.srccat.{ Main, Utils }
+import org.scalatest.{ Matchers, WordSpecLike }
+
+import scala.sys.process._
+
+/**
+ * Created by dengels on 03/05/15.
+ */
+class SystemSpec extends WordSpecLike with Matchers {
+
+  "The system" should {
+
+    "Have the correct size for small text files" in {
+
+      val loremIpsum = new LoremIpsum()
+      val rootDir = Files.createTempDirectory("rootDir")
+      val langDir = Files.createDirectory(rootDir.resolve("lang"))
+      val ownerDir = Files.createDirectory(langDir.resolve("owner"))
+      val files = for (i <- 1 to 10) yield {
+        val repo = Files.createDirectory(ownerDir.resolve(s"repo_$i"))
+        val file = Files.createFile(repo.resolve("file"))
+        val bytes = loremIpsum.getParagraphs(i).getBytes("UTF-8")
+        Files.write(file, bytes)
+      }
+
+      val totalSize = files.map { file => Files.size(file) }.sum
+
+      val resDir = Files.createTempDirectory("resDir")
+
+      Main.main(Array("-j", "1") ++ Array(rootDir, resDir).map(_.toAbsolutePath.toString))
+
+      val blobs = resDir.toFile.listFiles()
+
+      blobs.size should equal(1)
+
+      val resSize = Files.size(blobs(0).toPath)
+
+      resSize should be > totalSize
+
+    }
+
+    "Be able to read tarball repos" in {
+
+      val loremIpsum = new LoremIpsum()
+      val rootDir = Files.createTempDirectory("rootDir")
+      val langDir = Files.createDirectory(rootDir.resolve("lang"))
+      val ownerDir = Files.createDirectory(langDir.resolve("owner"))
+      val repo = Files.createDirectory(ownerDir.resolve(s"repo"))
+      val files = for (i <- 1 to 10) yield {
+        val file = Files.createFile(repo.resolve(s"file$i"))
+        val bytes = loremIpsum.getParagraphs(i).getBytes("UTF-8")
+        Files.write(file, bytes)
+      }
+
+      val totalSize = files.map { file => Files.size(file) }.sum
+
+      /* Create tar */
+      val command = Seq("tar", "-cf", "repo.tar", repo.getFileName.toString)
+      val rv = Process(command, cwd = Some(ownerDir.toFile)).!
+
+      rv should be(0)
+
+      println(s"Created tarball : ${ownerDir.resolve("repo.tar")}")
+
+      for (file <- Utils.listFilesRec(repo)) Files.delete(file)
+      Files.delete(repo)
+
+      val resDir = Files.createTempDirectory("resDir")
+
+      Main.main(Array("-j", "1") ++ Array(rootDir, resDir).map(_.toAbsolutePath.toString))
+
+      val blobs = resDir.toFile.listFiles()
+
+      blobs.size should equal(1)
+
+      val resSize = Files.size(blobs(0).toPath)
+
+      resSize should be > totalSize
+
+    }
+
+  }
+}
