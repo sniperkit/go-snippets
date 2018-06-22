0f5821e8ac727a537bb249bb4036ec55c18ca0b6
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-22 13:54:36 +0200

    Squashed '.snippets/github.com/DevMine/srcanlzr/' content from commit 620ce17796
    git-subtree-dir: .snippets/github.com/DevMine/srcanlzr
    git-subtree-split: 620ce17796481af550e429ec6b3e2e120ba9e3cd

---
+++ src/example_test.go
@@ -0,0 +1,32 @@
+// Copyright 2014-2015 The DevMine Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package src_test
+
+/*func ExampleUnmarshal() {
+	bs := []byte(`{"name": "foo", "languages": [], "packages": [], "loc": 0}`)
+	p, err := src.Unmarshal(bs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v\n", *p)
+}
+
+func ExampleMarshal() {
+	p := &src.Project{
+		Name:     "foo",
+		Repo:     nil,
+		RepoRaw:  nil,
+		Langs:    []*src.Language{},
+		Packages: []*src.Package{},
+		LoC:      0,
+	}
+
+	bs, _ := src.Marshal(p)
+
+	fmt.Println(string(bs))
+	// Output:
+	// {"name":"foo","repository":null,"languages":[],"packages":[],"loc":0}
+}*/
