b939849ec55a51fe412458b52ef03e5b9d540745
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-23 09:17:08 +0200

    Squashed 'dotfiles/github.com/pragmaticivan/dotfiles/' content from commit fc17b46c91
    git-subtree-dir: dotfiles/github.com/pragmaticivan/dotfiles
    git-subtree-split: fc17b46c91f709b38ce8d07b68b749df0df3f610

---
+++ vscode/install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+if test "$(which code)"; then
+	if [ "$(uname -s)" = "Darwin" ]; then
+		VSCODE_HOME="$HOME/Library/Application Support/Code"
+	else
+		VSCODE_HOME="$HOME/.config/Code"
+	fi
+
+	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+	ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+	ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+
+	# from `code --list-extensions`
+	modules="
+Borke.puppet
+CoenraadS.bracket-pair-colorizer
+EditorConfig.EditorConfig
+HookyQR.beautify
+PeterJausovec.vscode-docker
+be5invis.toml
+caarlos0.language-prometheus
+carolynvs.dep
+esbenp.prettier-vscode
+formulahendry.auto-close-tag
+formulahendry.auto-rename-tag
+foxundermoon.shell-format
+haaaad.ansible
+ipedrazas.kubernetes-snippets
+lukehoban.Go
+mauve.terraform
+ms-python.python
+octref.vetur
+patbenatar.advanced-new-file
+pnp.polacode
+rebornix.Ruby
+shanoor.vscode-nginx
+shinnn.alex
+teabyii.ayu
+timonwong.shellcheck
+"
+	for module in $modules; do
+		code --install-extension "$module" || true
+	done
+fi
